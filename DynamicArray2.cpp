74f51b3720bd05536bbd8790a5332e2694fabd58
ALKorzh <alexkorzhwork@gmail.com>
2024-03-14 21:23:12 +0300

    add func differentLetters

--- DynamicArray2.cpp
+++ DynamicArray2.cpp
@@ -1,7 +1,38 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+int differentLetters(int length, string input) {
+    char* symbols{ new char[length]{} };
+    int counter = 0;
+    for (int i = 0; i < length; i++) {
+        symbols[i] = input[i];
+    }
+    
+    for (int j = 0; j < length; j++) {
+        bool flag = true;
+        for (int k = 0; k < j; k++) {
+            if (symbols[j] == symbols[k]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            counter++;
+        }
+    }
+
+    delete[] symbols;
+    return counter;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    string input;
+    cout << "Enter text:" << endl;
+    cin >> input;
+    int InputLength = input.length();
+
+    cout << differentLetters(InputLength, input) << endl;
 }
 
